8bf33c45b8b4de544ed45adec6a18b2f77ed2bd0
dokibacsi <kvalcsi02@gmail.com>
2024-11-19 14:19:42 +0100

    kiindul alapv2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { ListGroup } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Autó webáruház</h1>
       </header>
+      <main>
+        <ListGroup>
+          <ListGroup.Item>Public</ListGroup.Item>
+          <ListGroup.Item>Admin</ListGroup.Item>
+        </ListGroup>
+        
+      </main>
     </div>
   );
 }
