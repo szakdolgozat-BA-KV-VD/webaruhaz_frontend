f69b8aab331e2935503053edf9f626747b3caacf
BeldyAdam <beldy.adam@gmail.com>
2024-12-03 15:45:31 +0100

    Kosár hozzadas es eltavolitas style hatravan

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/css/bootstrap.min.css"
-import { APIProvider } from './Contexts/APIContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { APIProvider } from "./Contexts/APIContext";
+import { KosarProvider } from "./Contexts/KosarContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <APIProvider><App /></APIProvider>
+  
+      <APIProvider>
+        <App />
+      </APIProvider>
+   
   </React.StrictMode>
 );
 
